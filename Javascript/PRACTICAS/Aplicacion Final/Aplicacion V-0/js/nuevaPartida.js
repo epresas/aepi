c5fa53fd295d9b216ae91a06125a8503a76a0323
epresas <ednmj1@gmail.com>
2017-12-28 21:11:17 +0100

    app final v0

---
+++ Javascript/PRACTICAS/Aplicacion Final/Aplicacion V-0/js/nuevaPartida.js
@@ -0,0 +1,21 @@
+
+/*
+
+La validación del formulario debe garantizar la validez de los datos:
+
+- Campo Nombre: seis caracteres mínimo, sin espacios
+- Campo Contraseña: seis caracteres mínimo incluyendo mayúscula/s, minúscula/s y número/s
+- Campo Repite la Contraseña: debe coincidir con la anterior
+- Campo Email: debe contener una arroba y un punto
+- Campo DNI: 8 dígitos y una mayúscula
+- Campo Fecha de nacimiento: una fecha en formato dd/mm/aaaa, NO MENORES DE EDAD
+- Radios País de residencia: alguno debe estar marcado
+- Lista Forma de Pago: alguna debe estar marcada
+- Checkbox Condiciones: debe estar marcado
+
+*/
+
+document.getElementById('validarFormularioPartida').onclick = function() {
+
+    alert("¡Ánimo!");
+}
