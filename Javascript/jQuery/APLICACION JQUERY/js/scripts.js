82db4a0edaee09c1edce2732cca6860a4b093f59
epresas <ednmj1@gmail.com>
2018-03-24 13:32:22 +0100

    Primera clase MEAN

--- Javascript/jQuery/APLICACION JQUERY/js/scripts.js
+++ Javascript/jQuery/APLICACION JQUERY/js/scripts.js
@@ -11,4 +11,27 @@ $(window).on('scroll',function () {
     $('#noria').css('transform','rotate('+userScroll *.05+'deg)');
 
     // console.log(userScroll);
-})
+
+$('.reveal').each(function () {
+    if($(window).scrollTop() > ($(this).offset().top - $(window).innerHeight()/ 1.5)){
+        $(this).addClass('visible');
+    }
+});
+
+});
+$(document).ready(function () {
+    $('.desplazar').on('click',scrollTo);
+
+
+});
+function scrollTo() {
+
+    let target = $(this).data('nivel');
+    let targetDepth = $(target).offset().top;
+    let windowDepth = $(window).scrollTop();
+    let timing = Math.abs(windowDepth - targetDepth ) * .9;
+    // alert(timing)
+    $('html').animate({
+        scrollTop:targetDepth
+    },timing);
+}
